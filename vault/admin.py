046ae2b9d2216f70442a8670e18a1b86bac99208
Everest1508 <riteshmahale15@gmail.com>
2024-08-10 07:23:55 +0000

    cc

--- vault/admin.py
+++ vault/admin.py
@@ -3,15 +3,20 @@ from .models import SSHConfig, AccessToken, FreeServer
 
 
 class UserRestrictedAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs  # Superusers see all data
+        return qs.filter(user=request.user)  # Non-superusers see only their data
+
     def formfield_for_manytomany(self, db_field, request, **kwargs):
         if db_field.name == "user" and not request.user.is_superuser:
-            kwargs["queryset"] = kwargs["queryset"].filter(
-                id=request.user.id)  # Restrict to current user
+            kwargs["queryset"] = kwargs["queryset"].filter(id=request.user.id)
         return super().formfield_for_manytomany(db_field, request, **kwargs)
 
     def get_readonly_fields(self, request, obj=None):
         if not request.user.is_superuser:
-            return ['user']  # Make 'user' field readonly for non-superusers
+            return ['user',]  # Make 'user' field readonly for non-superusers
         return super().get_readonly_fields(request, obj)
 
 
@@ -20,10 +25,9 @@ class SSHConfigAdmin(UserRestrictedAdmin):
     search_fields = ('name', 'host', 'username')
 
 
-admin.site.register(SSHConfig, SSHConfigAdmin)
-
-# Register your models here.
-admin.site.register(AccessToken, UserRestrictedAdmin)
+class AccessTokenAdmin(UserRestrictedAdmin):
+    list_display = ('name', 'expires_in')
+    search_fields = ('name',)
 
 
 @admin.register(FreeServer)
@@ -32,7 +36,6 @@ class FreeServerAdmin(UserRestrictedAdmin):
     search_fields = ('name', 'username', 'email', 'api_link')
     list_filter = ('renovation_date',)
     ordering = ('name',)
-
     fieldsets = (
         (None, {
             'fields': ('name', 'username', 'email', 'password')
@@ -44,3 +47,8 @@ class FreeServerAdmin(UserRestrictedAdmin):
             'fields': ('renovation_date',)
         }),
     )
+
+
+# Register your models here.
+admin.site.register(SSHConfig, SSHConfigAdmin)
+admin.site.register(AccessToken, AccessTokenAdmin)
